fcd1858a404c7262d5ae36d142888fc4e86d9179
Edward Yun <eyun1@mail.sfsu.edu>
2021-01-31 18:54:41 -0800

    Kenny is cool

--- scartch.c
+++ scartch.c
@@ -2,7 +2,7 @@ int print_all()
 {
     int x = 1;
     printf("%i", x);
-    return x;
+    return 1;
 }
 
 int main()
@@ -12,17 +12,3 @@ int main()
     printf("%s\n", "Hello world");
     return 0;
 }
-
-/***
- *adfasdf a asdfasdfasdf 
- * asdf
- * asdf
- * asdfasdfasd
-asdfasdfadfasdfasdfasdf
-asdf
-as
-df
-asdf
-as
-dfa
-*/
